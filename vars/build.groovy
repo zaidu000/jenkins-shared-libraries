531f3d260f8525d421c09f9876897711e9bf9a65
Mohammad Zaid Arif <92888846+zaidu000@users.noreply.github.com>
2025-08-14 23:43:21 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,3 +1,12 @@
-def call(String ProjectName,String ImageTag,String DockerHubUser){
-        sh "sudo docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(Map config = [:]) {
+    def imageName = config.imageName ?: error("Image name is required")
+    def imageTag = config.imageTag ?: 'latest'
+    def dockerfile = config.dockerfile ?: 'Dockerfile'
+    def context = config.context ?: '.'
+    
+    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
+    
+    sh """
+        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+    """
 }
