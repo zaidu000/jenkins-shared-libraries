5f9e67fb16e2f755e8facf549ffc41cd633365b4
Mohammad Zaid Arif <92888846+zaidu000@users.noreply.github.com>
2025-08-14 23:52:48 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,12 +1,10 @@
-def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
-    def dockerfile = config.dockerfile ?: 'Dockerfile'
-    def context = config.context ?: '.'
-    
-    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
-    
+def call(String imageName, String imageTag, String dockerUser) {
+    if (!imageName) { error "Image name is required" }
+    if (!imageTag) { imageTag = "latest" }
+
+    echo "Building Docker image: ${dockerUser}/${imageName}:${imageTag}"
+
     sh """
-        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+        docker build -t ${dockerUser}/${imageName}:${imageTag} -t ${dockerUser}/${imageName}:latest .
     """
 }
